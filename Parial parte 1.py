3560d5bb08a7873875705e753139887ceaa5d8fc
tomas <tomas27@gmail.com>
2025-07-28 18:43:39 -0600

    2

--- Parial parte 1.py
+++ Parial parte 1.py
@@ -1,1 +1,19 @@
 """PARCIAL PRIMRERA PARTE"""
+empleados = {}
+opcion = 0
+
+while opcion!=4:
+    print("+++ MENU+++")
+    print("1. Registrar emplados")
+    print("2. Mostar empleados")
+    print("3. Buscar empleados")
+    print("4. Salir")
+    try:
+        opcion= int(input("Ingrese su opcion: "))
+    except ValueError:
+        print("ERROR, ingrse un dato valido...")
+
+    match opcion:
+        case 1:
+            cantidad = int(input("¿Cuantos empleados desea registrar?: "))
+            for i in range (cantidad):
